cc802eceb9aa51d142aa263f7c7e3024b83f86a7
Mauricio Fuentes <mauriciofb@gmail.com>
2024-08-23 16:38:24 -0400

    Desafio Opcional
    Nuestro mounstruo de Frankenstein

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import FormCompetencias from './components/FormCompetencias.vue'
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <FormCompetencias />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
